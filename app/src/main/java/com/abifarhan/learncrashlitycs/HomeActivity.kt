c81b686f1700873f1aa46c2228e264f43d0e07c5
abiFarhan <abifarhan04@gmail.com>
2021-10-17 13:58:28 +0700

    Create options menu

--- app/src/main/java/com/abifarhan/learncrashlitycs/HomeActivity.kt
+++ app/src/main/java/com/abifarhan/learncrashlitycs/HomeActivity.kt
@@ -1,11 +1,28 @@
 package com.abifarhan.learncrashlitycs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class HomeActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_tweet -> startActivity(Intent(this, UpdateActivity::class.java))
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/abifarhan/learncrashlitycs/UpdateActivity.kt
+++ app/src/main/java/com/abifarhan/learncrashlitycs/UpdateActivity.kt
@@ -2,10 +2,27 @@ package com.abifarhan.learncrashlitycs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.abifarhan.learncrashlitycs.databinding.ActivityUpdateBinding
 
 class UpdateActivity : AppCompatActivity() {
+    private var _binding:ActivityUpdateBinding? = null
+
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_update)
+        _binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnUpload.setOnClickListener {
+            val tweet = binding.edtTweetText.text.toString().trim()
+
+
+        }
+
     }
+
+
 }
