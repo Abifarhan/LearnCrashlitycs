539dbc6a05e01a723210964fb7ee67b1d4e7782a
abiFarhan <abifarhan04@gmail.com>
2021-10-17 14:11:58 +0700

    Writing data to Database

--- app/src/main/java/com/abifarhan/learncrashlitycs/UpdateActivity.kt
+++ app/src/main/java/com/abifarhan/learncrashlitycs/UpdateActivity.kt
@@ -2,9 +2,14 @@ package com.abifarhan.learncrashlitycs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import com.abifarhan.learncrashlitycs.databinding.ActivityUpdateBinding
+import com.google.firebase.Timestamp
+import com.google.firebase.Timestamp.now
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class UpdateActivity : AppCompatActivity() {
     private var _binding:ActivityUpdateBinding? = null
@@ -18,8 +23,21 @@ class UpdateActivity : AppCompatActivity() {
 
         binding.btnUpload.setOnClickListener {
             val tweet = binding.edtTweetText.text.toString().trim()
+            val currentUser = FirebaseAuth.getInstance().currentUser?.email
+            val date = Timestamp.now()
 
+            val dataMap = hashMapOf<String, Any>()
+            dataMap["tweet"] = tweet
+            dataMap["user"] = currentUser!!
+            FirebaseFirestore.getInstance().collection("Posts")
+                .add(dataMap)
+                .addOnSuccessListener {
+                    finish()
+                }
 
+                .addOnFailureListener {
+                    Log.d("this","your error is ${it.localizedMessage }")
+                }
         }
 
     }
